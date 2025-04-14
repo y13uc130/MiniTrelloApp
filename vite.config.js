56bbec9f79b55f444063e38939173bd78f008809
intel.kancharla <intel.kancharla@gameskraft.com>
2025-04-14 14:01:09 +0530

    updated base on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   esbuild: {
     loader: 'jsx', // 🛠 Force JSX support in `.js` files
     include: /src\/.*\.(js|jsx)$/, // only apply to files in your `src/` folder
